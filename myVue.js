619de905e67d2664e33722c2d1308c04fc317454
xiongchenf <837195936@qq.com>
2018-09-18 10:09:54 +0800

    myVue

---
+++ myVue.js
@@ -0,0 +1,60 @@
+class Vue {
+    constructor (o) {
+        if (!o) return;
+        if (o.data) {
+            this.$init(o.data)
+        }
+        if (o.methods) {
+            this.$methods(o.methods);
+        }
+
+        if(o.el) {
+            this.$setEl(o.el);
+        }
+    }
+
+    $init(obj) {
+        for (let i in obj) {
+            let o = {};
+                o[i] = obj[i];
+            Object.defineProperty(this, i, {
+                set: function(val) {
+                    return o[i] = val;
+                },
+                get: function() {
+                    return o[i];
+                }
+            });
+        }
+    }
+
+    $methods(obj) {
+        for (let i in obj) {
+            console.log(obj[i]);
+        }
+    }
+
+    $setEl(el) {
+        if (!this.$el) {
+            el = el.substring(1);
+            this.$el = document.getElementById(el);
+        }
+        this.$render();
+    }
+
+    $render() {
+        let html = this.$el.outerHTML;
+        console.log(html);
+        let doms = [];
+        
+    }
+}
+
+
+
+
+
+
+
+
+
