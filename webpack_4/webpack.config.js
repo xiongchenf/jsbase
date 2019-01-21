14d101aaeacf79e1aeb89c69be41731ac708cccf
xiongchenf <837195936@qq.com>
2019-01-21 16:17:49 +0800

    defineProperty

--- webpack_4/webpack.config.js
+++ webpack_4/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const UglifyPlugin = require('uglifyjs-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-cosnt ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     // 输入
