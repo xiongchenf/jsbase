aad25be0dbcb2c9d1597a547f0a8d0874d1a4ea7
bear.xiong <bear.xiong@caibeitv.com>
2019-06-19 10:05:47 +0800

    long

--- ImageLoader.js
+++ ImageLoader.js
@@ -1,79 +1,21 @@
-;(function (win) {
-    function ImageLoader() {
-        let arr = [];
-        this.insert = (item) => {
-            arr.push(item);
-        };
-
-        this.init = (o) => {
-            let that = this;
-            let res = [];
-            loop(o, res);
-            res.map((v) => {
-                that.insert(new Img(v));
-            });
-        };
-
-        this.load = (callback) => {
-            let that = this;
-            arr.map((v) => {
-                console.log(v);
-                v.img.src = v.src;
-            });
-            let flag = true,
-                timer = setInterval(() => {
-                    let res = this.isLoaded();
-                    callback(res);
-                    if(res.status) {
-                        clearTimeout(timer);
-                    }
-                }, 200);
-
-            setTimeout(() => {
-                clearTimeout(timer);
-            }, 10000);
-        };
-
-        this.isLoaded = () => {
-            let status = true, count = 0;
-            arr.map((v) => {
-                if (!v.status) {
-                    status = false;
-                } else {
-                    count += 1;
-                }
-            });
-            return {
-                status: status,
-                count: count,
-                totalCount: arr.length
-            };
-        };
-
-        let Img = function(src) {
-            this.img = new Image();
-            this.src = src;
-            this.status = false;
-            this.fail = true;
-            this.img.onload = () => {
-                this.status = true;
-            }
-            this.img.onerror = () => {
-                this.fail = true;
-            }
-        }
-
-        let loop = (o, res) => {
-            let tem = Object.keys(o);
-            tem.map(v => {
-                if (typeof o[v] === 'object') {
-                    loop(o[v], res);
-                } else {
-                    res.push(o[v]);
-                }
-            });
-        }
+/**
+ * 
+ * 微信小程序图片预加载
+ * @autor xiongchenf
+ * 
+*/
+class ImageLoader {
+    constructor({ base, source, loading, loaded }) {
+        this.base = base;
+        this.source = source;
+        this.loading = loading;
+        this.loaded = loaded;
+        this._init();
     }
 
-    win.ImageLoader = ImageLoader;
-})(window);
+    _init() {
+        console.log(1);
+    }
+}
+
+export default new ImageLoader();
